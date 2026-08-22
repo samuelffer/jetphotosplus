ae388c213947f73575b7b3b386a592f5e77bb377
samuelffer <samuelferreiraouk@gmail.com>
2026-08-22 16:09:57 -0300

    Add photo assets to JPP_PHOTOS array

---
+++ docs/assets/photos.js
@@ -0,0 +1,8 @@
+window.JPP_PHOTOS = [
+  "assets/photos/1062694_1710041359.jpg",
+  "assets/photos/367796_1719283968.jpg",
+  "assets/photos/399698_1748802265.jpg",
+  "assets/photos/464190_1717195741.jpg",
+  "assets/photos/628242_1727738026.jpg",
+  "assets/photos/893838_1759625036.jpg"
+];
