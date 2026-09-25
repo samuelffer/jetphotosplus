d2bc066f876ca6c8d3fb978f1cb0a853c254e5fc
samuelffer <samuelferreiraouk@gmail.com>
2026-09-25 18:14:25 -0300

    removed the site of extension repository, moved to "jetphotosplus-site" repository
    new link for site repository: https://github.com/samuelffer/jetphotosplus-site

--- docs/assets/photos.js
+++
@@ -1,9 +0,0 @@
-// Generated automatically. Run update_photos.bat (Windows) or `node build-photo-manifest.mjs` (Mac/Linux) after editing assets/photos/.
-window.JPP_PHOTOS = [
-  "assets/photos/1062694_1710041359.jpg",
-  "assets/photos/367796_1719283968.jpg",
-  "assets/photos/399698_1748802265.jpg",
-  "assets/photos/464190_1717195741.jpg",
-  "assets/photos/628242_1727738026.jpg",
-  "assets/photos/893838_1759625036.jpg"
-];
